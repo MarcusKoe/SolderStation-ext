77428bb1d41b40496b12f67824a5513e06174faf
MarcusKoe <MarcusKoe@users.noreply.github.com>
2016-06-22 21:01:17 +0200

    Create conftables.h

---
+++ Arduino/conftables.h
@@ -0,0 +1,14 @@
+const String Langs[][3] ={
+
+									{"Deutsch", "English", "None"},							// 0
+									{"Ist:", "Current:", "None"},							// 1
+									{"Soll:", "Target:", "None"},							// 2
+									{"Warte", "Sleep", "None"},								// 3
+									{"Bereitschaft", "StandBy", "None"},					// 4
+									{"in", "in", "None"},									// 5
+									{"seit", "since", "None"},								// 6
+									{"Tempabfall", "Temp drop", "None"},					// 7
+									{"Abschaltung", "Shutdown", "None"},					// 8
+									{"Spitze aus ", "Tip off", "None"},						// 9
+									{"s", "s", "None"}										// 10
+									};

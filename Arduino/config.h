79834bd68c1484b36e109e99850d50fa2e2eca43
MarcusKoe <MarcusKoe@users.noreply.github.com>
2016-09-04 17:31:05 +0200

    Update config.h

--- Arduino/config.h
+++ Arduino/config.h
@@ -2,7 +2,24 @@ unsigned long TimeStart = millis() ;
 unsigned long TimeNow ;
 
 
-int configuration[11] = {
+const String Langs[][3] ={
+
+									{"Deutsch", "English", "None"},							// 0
+									{"Ist:", "Current:", "None"},							// 1
+									{"Soll:", "Target:", "None"},							// 2
+									{"Warte", "Sleep", "None"},								// 3
+									{"Bereitschaft", "StandBy", "None"},					// 4
+									{"in", "in", "None"},									// 5
+									{"seit", "since", "None"},								// 6
+									{"Tempabfall", "Temp drop", "None"},					// 7
+									{"Abschaltung", "Shutdown", "None"},					// 8
+									{"Spitze aus ", "Tip off", "None"},						// 9
+									{"s", "s", "None"}										// 10
+									};
+
+
+
+int configuration[14] = {
 							0,		// 0 Language - 0 Deutsch, 1 English
 							0,		// 1 Unit - 0 Celsuis, 1 Kelvin, 2 Fahrenheit
 							1,		// 2 Poti direction 0 or 1
@@ -13,32 +30,37 @@ int configuration[11] = {
 							255,	// 7 MaxPWM 0 - 255
 							450,	// 8 Maximum Temp
 							255,	// 9 StatusLEDBrightnes - Soldering
-							50		// 10 StatusLEDBrightnes - StandBy
+							50,		// 10 StatusLEDBrightnes - StandBy
+							250,	// 11 StandbyTemp
+							50,		// 12 Burntime, temperature increase during Burntime
+							4		// 13 Seconds Burntime
+
 } ;
 
 
 unsigned long IntervallArray[][2] = {
 
-									{0, 250},	// 0 Potitime
+									{0, 241},	// 0 Potitime
 									{0, 199},	// 1 TFTSolderscreen
-									{0, 200},	// 2 SetStandBy
-									{0, 1000},	// 3 SerialOutput
-									{0, 50},	// 4 Buttonstates
-									{0, 500},	// 5 Checkled
-									{0, 100},	// 6 TFTStandByScreen
-									{0, 30},	// 7 getTemperature
-									{0, 200},	// 8 set time
-									{0, 999},	// 9 StandBy clear
-									{0, 1000},	// 10 Buzzertime
+									{0, 197},	// 2 SetStandBy
+									{0, 997},	// 3 SerialOutput
+									{0, 47},	// 4 Buttonstates
+									{0, 503},	// 5 Checkled
+									{0, 97},	// 6 TFTStandByScreen
+									{0, 29},	// 7 getTemperature
+									{0, 211},	// 8 set time
+									{0, 991},	// 9 StandBy clear
+									{0, 1009},	// 10 Buzzertime
 									{0, 101},	// 11 WS2812b
-									{0, 2001}	// 12 EEprom saveconfig
+									{0, 1999},	// 12 EEprom saveconfig
+									{0, 73}		// 13 Burntime
 								};
 
 
 
 int PotiDelta = 0 ;
 int PotiDeltaPM = 5 ; // Value plus minus
-int MaxTemp = 450 ; // Be carefull!
+//int MaxTemp = 450 ; // Be carefull!
 
 //int MaxPwm = 255 ; //0 - 255
 bool HeaterOn = false ;
@@ -61,6 +83,7 @@ int TFToldPresets[9] = {999, 999, 999, 999, 999, 999, 999, 999, 999} ;
 int TFToldClock[5] = {0,0,0,0,0} ;
 
 int TimesArray[7] = {0, 0, 0, 0, 0, 0, 0} ; // hour, month, second, day, month, year, temp
+unsigned long TimeEpoch = 0 ;	// Unixtime
 
 
 int ArrayButtons[4][5] = {
@@ -78,7 +101,11 @@ int statusledarray[] = {0, 0, 0, 0, 0, 0} ; // 0 - HSVPos
 
 int ButtonValues[4] = {780, 680, 585, 400} ;
 
+byte Burntimestate = 0 ;
+unsigned long Burntimes[3] = {0, 0, 0} ;
 
+byte SetTimestate = 0 ;
+unsigned long SetTimetimes[3] = {0, 0, 0} ;
 
 
 
